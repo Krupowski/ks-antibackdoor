e1d7885b1cc37a2249c216f1685780c76ac1779b
Krupowski <fqbixirys@gmail.com>
2024-10-19 17:18:58 +0200

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,63 +1,6 @@
 local targetWords = {"https://", "PerformHttpRequest", "GetConvar", "print", "execute", "command", "txAdmin"}
 local foundScripts = {}
 
-function printColored(text, color)
-    local colorCode = {
-        red = "^1",
-        green = "^2",
-        yellow = "^3",
-        blue = "^4",
-        lightblue = "^5",
-        purple = "^6",
-        white = "^7",
-        black = "^8"
-    }
-    
-    local code = colorCode[color] or ""
-    print(code .. text)
-end
-
-
-function scanScriptsForResource(resourceName)
-    local numFiles = GetNumResourceMetadata(resourceName, "server_script") or 0
-    for j = 0, numFiles - 1 do
-        local luaFilePath = GetResourceMetadata(resourceName, "server_script", j)
-        if luaFilePath and not foundScripts[luaFilePath] then
-            local fileContent = LoadResourceFile(resourceName, luaFilePath)
-            if not fileContent then return end
-            
-            local lines = split(fileContent, "\n") 
-            for lineNum, line in ipairs(lines) do
-                for _, targetWord in ipairs(targetWords) do
-                    if line:find(targetWord) then
-                        foundScripts[luaFilePath] = true
-                        printColored("[script:" .. resourceName .. "] Found Word: " .. targetWord, "yellow")
-                        local encodedSnippet = json.encode(line) 
-                        printColored("Code Snippet (JSON): " .. encodedSnippet, "lightblue")
-                    end
-                end
-            end
-        end
-    end
-end
-
-function split(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = {}
-    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
-        table.insert(t, str)
-    end
-    return t
-end
-
-local resources = GetNumResources()
-for i = 0, resources - 1 do
-    local resourceName = GetResourceByFindIndex(i)
-    scanScriptsForResource(resourceName)
-end
-
 local Shared = {
     Enable = true,
     DiscordAnnounceDetection = true,
@@ -75,59 +18,80 @@ local Shared = {
     }
 }
 
-AddEventHandler('onResourceStart', function(res)
-    if GetCurrentResourceName() ~= res or not Shared.Enable then return end
-    
-    local detectedResources = scanForBackdoors()
+local function split(inputstr, sep)
+    sep = sep or "%s"
+    local t = {}
+    for str in inputstr:gmatch("([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
 
-    if #detectedResources > 0 then
-        if Shared.ConsolePrint then 
-            print("^1[DEBUG]^0 Found Backdoor in: ")
-            for _, v in pairs(detectedResources) do
-                print("^1[DEBUG]^0 Resource: " .. v.resource .. ", Detected String: " .. v.stringFound)
+local function logDetection(resource, targetWord, snippet, color)
+    print(("^3[script:%s]^7 Found Word: %s"):format(resource, targetWord))
+    print(("^5Code Snippet (JSON): %s"):format(json.encode(snippet)))
+end
+
+local function scanFile(resourceName, luaFilePath)
+    local fileContent = LoadResourceFile(resourceName, luaFilePath)
+    if not fileContent then return end
+
+    for lineNum, line in ipairs(split(fileContent, "\n")) do
+        for _, targetWord in ipairs(targetWords) do
+            if line:find(targetWord) then
+                foundScripts[luaFilePath] = true
+                logDetection(resourceName, targetWord, line, "yellow")
+                break
             end
         end
+    end
+end
 
-        if Shared.DiscordAnnounceDetection and Shared.DiscordWebhook ~= "" then 
-            sendToDiscord(detectedResources)
-        end 
-
-        if Shared.StopServer then 
-            Citizen.Wait(2000)
-            os.exit()
+local function scanScriptsForResource(resourceName)
+    local numFiles = GetNumResourceMetadata(resourceName, "server_script") or 0
+    for j = 0, numFiles - 1 do
+        local luaFilePath = GetResourceMetadata(resourceName, "server_script", j)
+        if luaFilePath and not foundScripts[luaFilePath] then
+            scanFile(resourceName, luaFilePath)
         end
     end
-end)
+end
 
-function scanForBackdoors()
+local function scanForBackdoors()
     local detectedResources = {}
 
     for i = 0, GetNumResources() - 1 do
         local resourceName = GetResourceByFindIndex(i)
         if resourceName ~= GetCurrentResourceName() then
             local numFiles = GetNumResourceMetadata(resourceName, 'server_script')
-            for j = 0, numFiles-1 do
+            for j = 0, numFiles - 1 do
                 local filePath = GetResourceMetadata(resourceName, 'server_script', j)
                 local fileContent = LoadResourceFile(resourceName, filePath)
-                
-                for _, str in ipairs(Shared.BackdoorStrings) do
-                    if fileContent and string.find(fileContent, str) then
-                        table.insert(detectedResources, {resource = resourceName .. '/' .. filePath, stringFound = str})
+
+                if fileContent then
+                    for _, backdoorString in ipairs(Shared.BackdoorStrings) do
+                        if fileContent:find(backdoorString) then
+                            table.insert(detectedResources, {resource = resourceName .. '/' .. filePath, stringFound = backdoorString})
+                            break -- Znaleziono backdoor, przerwij dalsze sprawdzanie
+                        end
                     end
                 end
             end
         end
     end
+
     return detectedResources
 end
 
-function sendToDiscord(infectedResources)
+local function sendToDiscord(detectedResources)
+    if Shared.DiscordWebhook == "" then return end
+
     local descriptions = ""
-    for _, v in pairs(infectedResources) do
-        descriptions = descriptions .. "**Resource:** " .. v.resource .. " **Detected String:** " .. v.stringFound .. "\n"
+    for _, v in ipairs(detectedResources) do
+        descriptions = descriptions .. ("**Resource:** %s **Detected String:** %s\n"):format(v.resource, v.stringFound)
     end
 
-    local connect = {
+    local message = {
         {
             ["color"] = 16711680,
             ["title"] = "Wykryto Backdoora!",
@@ -136,6 +100,40 @@ function sendToDiscord(infectedResources)
         }
     }
 
-    PerformHttpRequest(Shared.DiscordWebhook, function(err, text, headers) end, 'POST', json.encode({username = "Anti Backdoor", embeds = connect}), {['Content-Type'] = 'application/json'})
+    PerformHttpRequest(Shared.DiscordWebhook, function(err, text, headers) end, 'POST', json.encode({username = "Anti Backdoor", embeds = message}), {['Content-Type'] = 'application/json'})
 end
 
+local function handleBackdoorDetection()
+    local detectedResources = scanForBackdoors()
+
+    if #detectedResources > 0 then
+        if Shared.ConsolePrint then
+            print("^1[DEBUG]^0 Found Backdoor in:")
+            for _, v in ipairs(detectedResources) do
+                print("^1[DEBUG]^0 Resource: " .. v.resource .. ", Detected String: " .. v.stringFound)
+            end
+        end
+
+        if Shared.DiscordAnnounceDetection then
+            sendToDiscord(detectedResources)
+        end
+
+        if Shared.StopServer then
+            Citizen.Wait(2000)
+            os.exit()
+        end
+    end
+end
+
+-- Triggered when a resource starts
+AddEventHandler('onResourceStart', function(res)
+    if GetCurrentResourceName() == res and Shared.Enable then
+        handleBackdoorDetection()
+    end
+end)
+
+-- Initial scan on all resources
+for i = 0, GetNumResources() - 1 do
+    local resourceName = GetResourceByFindIndex(i)
+    scanScriptsForResource(resourceName)
+end
